2f310961b13f4d1ba4a7579d9f36e3908f5758ec
Artem Osadchiy <osadchiyzp93@gmail.com>
2024-03-26 16:15:58 +0200

    App component update with zustand store

--- App.tsx
+++ App.tsx
@@ -1,103 +1,42 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {AuthStack, HomeStack} from './src/processes';
-import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
-
-type State = {
-  isLoading: boolean;
-  isSignout: boolean;
-  user: FirebaseAuthTypes.User | null;
-};
-
-type Action =
-  | {type: 'SIGN_IN'; user: FirebaseAuthTypes.User | null}
-  | {type: 'LOAD_FINISHED'}
-  | {type: 'SIGN_OUT'};
-
-type Reducer = (state: State, action: Action) => State;
-
-type AuthContextType = {
-  signIn: (user: FirebaseAuthTypes.User) => void;
-  signOut: () => void;
-  signUp: (user: FirebaseAuthTypes.User) => void;
-};
-
-export const AuthContext = React.createContext<AuthContextType | undefined>(
-  undefined,
-);
+import React, {useMemo} from 'react';
+import {AuthStack, HomeStack, ProfileStack} from './src/processes';
+//import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
+import {
+  checkUserFullfilled,
+  useUserStore,
+  useWatchUserAuth,
+} from './src/shared';
+import {useAppDataStore} from './src/shared/store/appData';
 
 function App(): JSX.Element | null {
-  const [state, dispatch] = React.useReducer<Reducer>(
-    (prevState, action) => {
-      switch (action.type) {
-        case 'SIGN_IN':
-          return {
-            ...prevState,
-            isSignout: false,
-            user: action.user,
-          };
-        case 'LOAD_FINISHED': {
-          return {
-            ...prevState,
-            isLoading: false,
-          };
-        }
-        case 'SIGN_OUT':
-          return {
-            ...prevState,
-            isSignout: true,
-            user: null,
-          };
-      }
-    },
-    {
-      isLoading: true,
-      isSignout: true,
-      user: null,
-    },
-  );
+  useWatchUserAuth();
+  const {isInitLoading} = useAppDataStore(state => ({
+    isInitLoading: state.isInitLoading,
+  }));
+  const {user} = useUserStore(state => ({user: state.user}));
 
-  const _handleAuthStateChanged = React.useCallback(
-    (user: FirebaseAuthTypes.User | null) => {
-      console.log('USER: ', user);
-      if (user) {
-        dispatch({type: 'SIGN_IN', user});
-      } else {
-        dispatch({type: 'SIGN_OUT'});
-      }
-      dispatch({type: 'LOAD_FINISHED'});
-    },
-    [],
-  );
+  const isUserFullfilled = useMemo(() => {
+    if (user) {
+      return checkUserFullfilled(user);
+    }
+    return false;
+  }, [user]);
 
-  React.useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(_handleAuthStateChanged);
-    return subscriber; // unsubscribe on unmount
-  }, []);
-
-  const authContext = React.useMemo<AuthContextType>(
-    () => ({
-      signIn: (user: FirebaseAuthTypes.User) => {
-        dispatch({type: 'SIGN_IN', user});
-      },
-      signOut: () => dispatch({type: 'SIGN_OUT'}),
-      signUp: (user: FirebaseAuthTypes.User) => {
-        dispatch({type: 'SIGN_IN', user});
-      },
-    }),
-    [],
-  );
-
-  if (state.isLoading) {
+  if (isInitLoading || user === undefined) {
     return null;
   }
 
   return (
-    <AuthContext.Provider value={authContext}>
-      <NavigationContainer>
-        {state.isSignout ? <AuthStack /> : <HomeStack />}
-      </NavigationContainer>
-    </AuthContext.Provider>
+    <NavigationContainer>
+      {user === null ? (
+        <AuthStack />
+      ) : isUserFullfilled ? (
+        <HomeStack />
+      ) : (
+        <ProfileStack />
+      )}
+    </NavigationContainer>
   );
 }
 
